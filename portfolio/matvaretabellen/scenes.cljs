4be4ccfdfdbacede43a6784cacd6c878cc1d980e
Magnar Sveen <magnar.sveen@mattilsynet.no>
2023-09-20 10:56:24 +0200

    Skill portfolio-scener for Matvaretabellen og designsystemet
    - så langt det lar seg gjøre

--- portfolio/matvaretabellen/scenes.cljs
+++ portfolio/matvaretabellen/scenes.cljs
@@ -1,14 +1,10 @@
 (ns matvaretabellen.scenes
   (:require [matvaretabellen.components.nutrition-table-scenes]
-            [mt-designsystem.components.breadcrumbs-scenes]
-            [mt-designsystem.components.search-input-scenes]
-            [mt-designsystem.components.site-header-scenes]
+            [mt-designsystem.scenes]
             [portfolio.ui :as ui]))
 
 :matvaretabellen.components.nutrition-table-scenes/keep
-:mt-designsystem.components.breadcrumbs-scenes/keep
-:mt-designsystem.components.search-input-scenes/keep
-:mt-designsystem.components.site-header-scenes/keep
+:mt-designsystem.scenes/keep
 
 (def app
   (ui/start!
