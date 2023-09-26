c1d2323593643a68d4d65be99620c66044abaa56
Christian Johansen <christian.johansen@mattilsynet.no>
2023-09-26 16:02:10 +0200

    Legg til egne databasetyper
    Co-authored-by: Magnar Sveen <magnar.sveen@mattilsynet.no>

---
+++ src/matvaretabellen/db.clj
@@ -0,0 +1,24 @@
+(ns matvaretabellen.db
+  (:require [datomic-type-extensions.api :as d]
+            [datomic-type-extensions.types :refer [define-dte]]))
+
+(define-dte :i18n/string :db.type/string
+  [this] (pr-str this)
+  [^String s] (read-string s))
+
+(define-dte :broch/quantity :db.type/string
+  [this] (pr-str this)
+  [^String s] (read-string s))
+
+(defn create-database [uri schema]
+  (d/create-database uri)
+  (let [conn (d/connect uri)]
+    @(d/transact conn schema)
+    conn))
+
+(defn get-i18n-attrs [db]
+  (d/q '[:find [?a ...]
+         :where
+         [?e :dte/valueType :i18n/string]
+         [?e :db/ident ?a]]
+       db))
