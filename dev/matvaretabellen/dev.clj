49db9f9b14e3eacfea4a19a6d1374774a8583731
Magnar Sveen <magnar.sveen@mattilsynet.no>
2023-10-02 08:48:37 +0200

    Legg til første konfigurasjon

--- dev/matvaretabellen/dev.clj
+++ dev/matvaretabellen/dev.clj
@@ -1,2 +1,18 @@
-(ns matvaretabellen.dev)
+(ns matvaretabellen.dev
+  (:require [confair.config :as config]
+            [confair.config-admin :as ca]))
 
+(def config (-> (config/from-file "./config/local-config.edn")
+                (config/mask-config)))
+
+(comment
+  (ca/conceal-value (config/from-file "./config/local-config.edn")
+                    :secret/dev
+                    :foodcase/bearer-token)
+
+  (ca/conceal-value (config/from-file "./config/prod-config.edn")
+                    :secret/prod
+                    :foodcase/bearer-token)
+
+  (:foodcase/bearer-token config)
+  )
