91eeb14e368b5ed87a8e8915bc1f434a8b91caa6
Christian Johansen <christian.johansen@mattilsynet.no>
2023-10-24 15:36:30 +0200

    Implementer site header i ny CSS

--- ui/portfolio/mt_designsystem/components/site_header_scenes.cljs
+++ ui/portfolio/mt_designsystem/components/site_header_scenes.cljs
@@ -3,4 +3,6 @@
             [portfolio.dumdom :as portfolio :refer [defscene]]))
 
 (defscene basic-header
+  :canvas/layout {:kind :rows
+                  :xs [{:viewport/padding [0 0 0 0]}]}
   (SiteHeader {:home-url "#"}))
