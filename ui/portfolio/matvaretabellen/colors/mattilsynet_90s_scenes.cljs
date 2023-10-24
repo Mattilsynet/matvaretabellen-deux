23ddcca93458e400b0e4ac9bb6ba440967652e60
Christian Johansen <christian.johansen@mattilsynet.no>
2023-10-24 08:15:44 +0200

    Legg til den game Mattilsynet fargepaletten

---
+++ ui/portfolio/matvaretabellen/colors/mattilsynet_90s_scenes.cljs
@@ -0,0 +1,35 @@
+(ns matvaretabellen.colors.mattilsynet-90s-scenes
+  (:require [clojure.string :as str]
+            [portfolio.dumdom :as portfolio :refer [defscene]]))
+
+(defn render-colors [color-list]
+  (->> color-list
+       (map (fn [color-name]
+              [:div {:style {:padding 20
+                             :margin "20px 0"
+                             :background (str "var(--" (name color-name) ")")}}
+               [:span {:style {:background "#fff"
+                               :display "inline-block"
+                               :padding "2px 6px"
+                               :border-radius 4
+                               :color "#000000"
+                               :box-shadow "0 1px 3px rgba(0, 0, 0, 0.2)"}}
+                (str "$" (str/replace (name color-name) #"-" "."))]]))))
+
+(defscene mt90s-colors
+  [:div
+   (render-colors
+    [:mt-color-red-100
+     :mt-color-red-500
+     :mt-color-beige-100
+     :mt-color-beige-200
+     :mt-color-beige-500
+     :mt-color-brown-900
+     :mt-color-blue-100
+     :mt-color-blue-200
+     :mt-color-blue-500
+     :mt-color-green-100
+     :mt-color-green-500
+     :mt-color-orange-100
+     :mt-color-orange-500
+     :mt-color-white])])
--- ui/portfolio/matvaretabellen/scenes.cljs
+++ ui/portfolio/matvaretabellen/scenes.cljs
@@ -1,10 +1,12 @@
 (ns matvaretabellen.scenes
-  (:require [matvaretabellen.components.nutrition-table-scenes]
+  (:require [matvaretabellen.colors.mattilsynet-90s-scenes]
+            [matvaretabellen.components.nutrition-table-scenes]
             [matvaretabellen.components.pie-chart-scenes]
             [matvaretabellen.components.toc-scenes]
             [mt-designsystem.scenes]
             [portfolio.ui :as ui]))
 
+:matvaretabellen.colors.mattilsynet-90s-scenes/keep
 :matvaretabellen.components.nutrition-table-scenes/keep
 :matvaretabellen.components.pie-chart-scenes/keep
 :matvaretabellen.components.toc-scenes/keep
@@ -13,5 +15,6 @@
 (defonce app
   (ui/start!
    {:config
-    {:css-paths ["/css/mt-designsystem.css"
+    {:css-paths ["/css/theme-mt90s.css"
+                 "/css/mt-designsystem.css"
                  "/css/matvaretabellen.css"]}}))
