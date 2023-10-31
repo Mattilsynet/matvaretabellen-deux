bc7ff16866cbcdc1548a2c5ef993ed15903282b1
Christian Johansen <christian.johansen@mattilsynet.no>
2023-10-31 14:14:38 +0100

    Ny CSS på matgruppe-siden

--- src/matvaretabellen/pages/food_group_page.clj
+++ src/matvaretabellen/pages/food_group_page.clj
@@ -3,24 +3,25 @@
             [matvaretabellen.crumbs :as crumbs]
             [matvaretabellen.urls :as urls]
             [mmm.components.breadcrumbs :refer [Breadcrumbs]]
+            [mmm.components.footer :refer [SiteFooter]]
             [mmm.components.site-header :refer [SiteHeader]]))
 
 (defn render [context _db page]
   (let [food-group (d/entity (:foods/db context)
                              [:food-group/id (:food-group/id page)])
         locale (:page/locale page)]
-    [:html
+    [:html {:class "mmm"}
      [:body
       (SiteHeader {:home-url "/"})
-      [:div
-       [:div.mmm-hero-banner
-        [:div.container
-         (Breadcrumbs {:links (crumbs/crumble locale food-group)})]]
-       [:div.mmm-hero-banner
-        [:div.container
-         [:h1.h1 (get-in food-group [:food-group/name locale])]
-         [:div.mmm-cards.mtl
-          (for [child (:food-group/_parent food-group)]
-            (let [the-name (get-in child [:food-group/name locale])]
-              [:a.mmm-card {:href (urls/get-food-group-url locale the-name)}
-               the-name]))]]]]]]))
+      [:div.mmm-themed.mmm-brand-theme1
+       [:div.mmm-container.mmm-section
+        (Breadcrumbs {:links (crumbs/crumble locale food-group)})]
+       [:div.mmm-container.mmm-section.mmm-mtxl
+        [:h1.mmm-h1 (get-in food-group [:food-group/name locale])]
+        [:div.mmm-cards.mmm-block
+         (for [child (:food-group/_parent food-group)]
+           (let [the-name (get-in child [:food-group/name locale])]
+             [:a.mmm-card.mmm-link {:href (urls/get-food-group-url locale the-name)}
+              the-name]))]]]
+      [:div.mmm-container.mmm-section
+       (SiteFooter)]]]))
