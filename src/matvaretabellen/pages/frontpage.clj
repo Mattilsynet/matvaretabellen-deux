6746a396e22820a3078ea9ca19332fd3064a7207
Christian Johansen <christian.johansen@mattilsynet.no>
2023-10-31 13:33:49 +0100

    Bruk banner layout på forsiden

--- src/matvaretabellen/pages/frontpage.clj
+++ src/matvaretabellen/pages/frontpage.clj
@@ -14,61 +14,56 @@
      :href (urls/get-food-url locale food-name)}))
 
 (defn BananaTeaserBox [locale db]
-  [:div.one-two-grid
-   [:div.one-box
-    [:div.flex.flex-center.flex-gap-m.phm
-     [:img {:src "/images/banana.jpg"}]
-     [:div
+  [:div.mmm-banner-media-buttons.mmm-section
+   [:div.mmm-container
+    [:div.mmm-media
+     [:aside [:img.mmm-img {:src "/images/banana.jpg"}]]
+     [:article.mmm-text.mmm-tight
       [:h4 [:i18n ::did-you-know]]
-      [:p [:i18n ::banana-nutrition-facts] " "
-       [:a.nbr (get-food-info locale db "06.525")
-        [:i18n ::read-more-about-banana]]]]]]
-   [:a.two-box.pam {:href [:i18n ::crumbs/food-groups-url]}
-    [:div
-     [:p [:i18n ::all-food-groups]]
-     [:p [:i18n ::see-all-food-groups-overview]]]]
-   [:div.two-box.pam
-    [:div
-     [:p [:i18n ::all-nutrients]]
-     [:p [:i18n ::see-all-nutrients-overview]]]]])
+      [:p [:i18n ::banana-nutrition-facts]]
+      [:p [:a.mmm-nbr (get-food-info locale db "06.525")
+           [:i18n ::read-more-about-banana]]]]]
+    [:div.mmm-buttons.mmm-text.mmm-tight
+     [:a.mmm-banner-button {:href [:i18n ::crumbs/food-groups-url]}
+      [:p [:i18n ::all-food-groups]]
+      [:p [:i18n ::see-all-food-groups-overview]]]
+     [:a.mmm-banner-button {:href "/"}
+      [:p [:i18n ::all-nutrients]]
+      [:p [:i18n ::see-all-nutrients-overview]]]]]])
 
 (defn render [_context db page]
   (let [locale (:page/locale page)]
-    [:html
+    [:html {:class "mmm"}
      [:body
       (SiteHeader {:home-url "/"})
-      [:div
-       [:div.container.mtl
-        (Breadcrumbs
-         {:links (crumbs/crumble locale)})]
-       [:div.container.mtl
-        [:div.search-input-wrap
-         [:h1.h2.mbl [:i18n ::search-label]]
-         (SearchInput {:button {:text [:i18n ::search-button]}
-                       :input {:name "foods-search"}
-                       :autocomplete-id "foods-results"})]]
-       [:div.mmm-secondary-banner.mtxxl
-        [:div.container
-         (BananaTeaserBox locale db)]]
-       [:div.container.mtl
-        [:div.flex.flex-gap-l
-         (Toc {:title [:i18n ::common-food-searches]
-               :contents [{:title "Egg" :href "?search=egg"}
-                          {:title "Gulrot" :href "?search=gulrot"}
-                          {:title "Havregryn" :href "?search=havregryn"}
-                          {:title "Potet" :href "?search=potet"}]})
-         [:div {:style {:flex 1}}
-          (Toc {:title (list [:i18n ::new-in-food-table] " 2023")
-                :contents [{:title "Egyptisk tahini"
-                            :href "/"}
-                           {:title "Gyroskjøtt"
-                            :href "/"}
-                           {:title "Bratwurst pølser"
-                            :href "/"}
-                           {:title "Vitamin K"
-                            :href "/"}]})]
-         (Toc {:title [:i18n ::seasonal-goods]
-               :contents (for [id ["06.010" "06.003" "06.016" "06.055"]]
-                           (get-food-info locale db id))})]]
-       [:div.container.mtl
-        (SiteFooter)]]]]))
+      [:div.mmm-container.mmm-section
+       (Breadcrumbs
+        {:links (crumbs/crumble locale)})]
+      [:div.mmm-container-narrow.mmm-section.mmm-mbxxl
+       [:h1.mmm-h2.mmm-mbl [:i18n ::search-label]]
+       (SearchInput {:button {:text [:i18n ::search-button]}
+                     :input {:name "foods-search"}
+                     :autocomplete-id "foods-results"})]
+      (BananaTeaserBox locale db)
+      [:div.container.mtl
+       [:div.flex.flex-gap-l
+        (Toc {:title [:i18n ::common-food-searches]
+              :contents [{:title "Egg" :href "?search=egg"}
+                         {:title "Gulrot" :href "?search=gulrot"}
+                         {:title "Havregryn" :href "?search=havregryn"}
+                         {:title "Potet" :href "?search=potet"}]})
+        [:div {:style {:flex 1}}
+         (Toc {:title (list [:i18n ::new-in-food-table] " 2023")
+               :contents [{:title "Egyptisk tahini"
+                           :href "/"}
+                          {:title "Gyroskjøtt"
+                           :href "/"}
+                          {:title "Bratwurst pølser"
+                           :href "/"}
+                          {:title "Vitamin K"
+                           :href "/"}]})]
+        (Toc {:title [:i18n ::seasonal-goods]
+              :contents (for [id ["06.010" "06.003" "06.016" "06.055"]]
+                          (get-food-info locale db id))})]]
+      [:div.mmm-container
+       (SiteFooter)]]]))
