93915132cc0055bb053fd3a8d9c968700546c541
Christian Johansen <christian.johansen@mattilsynet.no>
2023-10-31 10:37:24 +0100

    Legg til en smal container

--- ui/portfolio/mmm/layouts/container_scenes.cljs
+++ ui/portfolio/mmm/layouts/container_scenes.cljs
@@ -14,3 +14,15 @@
    bredeste laget).")
    (e/p "Under ser du en hund som illustrerer bolkens bredde.")
    (e/p (e/img {:src "/images/dog.jpg"}))])
+
+(defscene container-narrow
+  ".mmm-container-narrow er en litt smalere variant av samme komponent."
+  [:div.mmm-container-narrow
+   (e/h1 "Jeg er i en smal container")
+   (e/p "Containeren sørger for at ikke innholdet renner over alle bauger og
+   kanter, og blir så bredt at det blir vanskelig å finne noen struktur. Denne
+   layouten brukes typisk ytterst for å sette rammene for hele sidens bredde, og
+   er ikke nødvendigvis egnet til å ha mye tekst i (da det fort blir i det
+   bredeste laget).")
+   (e/p "Under ser du en hund som illustrerer bolkens bredde.")
+   (e/p (e/img {:src "/images/dog.jpg"}))])
