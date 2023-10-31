e6c27e02b060c9494da979345bfc6a159ae3eb40
Christian Johansen <christian.johansen@mattilsynet.no>
2023-10-31 15:15:54 +0100

    Ikke lur meg til å starte et REPL fra en ødelagt classpath

--- ui/.dir-locals.el
+++
@@ -1,6 +0,0 @@
-((nil
-  (cider-clojure-cli-aliases . "-A:ui")
-  (cider-default-cljs-repl . figwheel-main)
-  (cider-preferred-build-tool . clojure-cli)
-  (cider-figwheel-main-default-options . ":ui")))
-
