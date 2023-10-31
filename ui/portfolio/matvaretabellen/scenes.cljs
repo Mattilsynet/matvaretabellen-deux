7d6a302b4dc9cb0c91813f69f427c4e9bc4ab70e
Magnar Sveen <magnar.sveen@mattilsynet.no>
2023-10-31 09:07:06 +0100

    Fjern lo, clojure-lsp krever ikke at defonce brukes

--- ui/portfolio/matvaretabellen/scenes.cljs
+++ ui/portfolio/matvaretabellen/scenes.cljs
@@ -1,14 +1,16 @@
 (ns matvaretabellen.scenes
   (:require [matvaretabellen.components.nutrition-table-scenes]
+            [matvaretabellen.components.pie-chart-scenes]
             [matvaretabellen.components.toc-scenes]
             [mt-designsystem.scenes]
             [portfolio.ui :as ui]))
 
 :matvaretabellen.components.nutrition-table-scenes/keep
+:matvaretabellen.components.pie-chart-scenes/keep
 :matvaretabellen.components.toc-scenes/keep
 :mt-designsystem.scenes/keep
 
-(def app
+(defonce app
   (ui/start!
    {:config
     {:css-paths ["/css/mt-designsystem.css"
