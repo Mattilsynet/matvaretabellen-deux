ce267e26974f426ff3768d74ad4024be551f96e8
Christian Johansen <christian.johansen@mattilsynet.no>
2023-10-31 15:16:20 +0100

    Den obligatoriske etter-rebase-rydd-opp-committen

--- ui/portfolio/matvaretabellen/scenes.cljs
+++ ui/portfolio/matvaretabellen/scenes.cljs
@@ -1,29 +1,12 @@
 (ns matvaretabellen.scenes
-  (:require [matvaretabellen.colors.mattilsynet-90s-scenes]
-            [matvaretabellen.components.button-scenes]
-            [matvaretabellen.components.nutrition-table-scenes]
+  (:require [matvaretabellen.components.nutrition-table-scenes]
             [matvaretabellen.components.pie-chart-scenes]
-            [matvaretabellen.components.search-input-scenes]
-            [matvaretabellen.components.text-input-scenes]
-            [matvaretabellen.components.toc-scenes]
-            [matvaretabellen.elements.image-scenes]
-            [matvaretabellen.elements.table-scenes]
-            [matvaretabellen.elements.typography-scenes]
-            [matvaretabellen.layouts.container-scenes]
             [mmm.scenes]
             [portfolio.data :as data]
             [portfolio.ui :as ui]))
 
 :matvaretabellen.components.nutrition-table-scenes/keep
 :matvaretabellen.components.pie-chart-scenes/keep
-:matvaretabellen.components.search-input-scenes/keep
-:matvaretabellen.components.text-input-scenes/keep
-:matvaretabellen.components.toc-scenes/keep
-:matvaretabellen.elements.image-scenes/keep
-:matvaretabellen.elements.table-scenes/keep
-:matvaretabellen.elements.typography-scenes/keep
-:matvaretabellen.layouts.container-scenes/keep
-:mt-designsystem.scenes/keep
 :mmm.scenes/keep
 
 (data/register-collection!
