d2ba16f6d805719598882f8d0cb1f4f3ecabc83e
Christian Johansen <christian.johansen@mattilsynet.no>
2023-10-31 14:45:45 +0100

    Legg til støtte for toppjustert media layout

--- ui/portfolio/mmm/layouts/media_scenes.cljs
+++ ui/portfolio/mmm/layouts/media_scenes.cljs
@@ -12,3 +12,16 @@
      [:p "Banan er den frukten med høyest innhold av karbohydrater blant
     fruktene i Matvaretabellen. Den har også et høyt innhold av magnesium."]
      [:p [:a {:href "#"} "Les mer om bananen her"]]]]))
+
+(defscene media-anchor-top
+  "`.mmm-media.mmm-media-at` ankrer teksten mot toppen av media-komponenten, i
+  stedet for midten."
+  (e/block
+   [:div.mmm-media.mmm-media-at
+    [:aside
+     [:img.mmm-img {:src "/images/banana.jpg"}]]
+    [:article.mmm-text.mmm-tight
+     [:h4 "Visste du at..."]
+     [:p "Banan er den frukten med høyest innhold av karbohydrater blant
+    fruktene i Matvaretabellen. Den har også et høyt innhold av magnesium."]
+     [:p [:a {:href "#"} "Les mer om bananen her"]]]]))
