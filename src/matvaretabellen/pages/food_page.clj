a2684731abc3dd39b87825fa243f34ec0bd1410d
Christian Johansen <christian.johansen@mattilsynet.no>
2023-11-03 13:50:40 +0100

    Dropp dobbel padding for passepartout på mobil
    Trenger ikke padding både utenfor og inni passepartout når horisontal plass er
    mangelvare

--- src/matvaretabellen/pages/food_page.clj
+++ src/matvaretabellen/pages/food_page.clj
@@ -78,7 +78,7 @@
          [:td cell])])]])
 
 (defn passepartout [& body]
-  [:div.mmm-container.mmm-section
+  [:div.mmm-container.mmm-section.mmm-mobile-phn
    [:div.mmm-passepartout
     [:div.mmm-container-focused.mmm-vert-layout-m
      body]]])
