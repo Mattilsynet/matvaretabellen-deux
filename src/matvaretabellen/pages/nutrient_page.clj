314d4bd9c49e81f2139fe23f343f31f0d5559701
Christian Johansen <christian.johansen@mattilsynet.no>
2023-11-08 14:29:23 +0100

    Fjern header og bak informasjon inn i tabellheadings på næringsstoffene

--- src/matvaretabellen/pages/nutrient_page.clj
+++ src/matvaretabellen/pages/nutrient_page.clj
@@ -1,7 +1,5 @@
 (ns matvaretabellen.pages.nutrient-page
-  (:require [broch.core :as b]
-            [clojure.string :as str]
-            [datomic-type-extensions.api :as d]
+  (:require [datomic-type-extensions.api :as d]
             [matvaretabellen.crumbs :as crumbs]
             [matvaretabellen.food :as food]
             [matvaretabellen.nutrient :as nutrient]
@@ -12,9 +10,9 @@
             [mmm.components.site-header :refer [SiteHeader]]))
 
 (defn prepare-foods-table [nutrient locale foods]
-  {:headers [{:text [:i18n ::food]}
-             {:text [:i18n :i18n/lookup (nutrient/get-name nutrient)]
-              :colspan 3}]
+  {:headers [{:text [:i18n ::food {:n (count foods)}]}
+             {:text [:i18n ::nutrient-header (nutrient/get-name nutrient)]
+              :class "mmm-tar mmm-nbr"}]
    :rows (for [food foods]
            [{:text [:a.mmm-link {:href (urls/get-food-url locale food)}
                     [:i18n :i18n/lookup (:food/name food)]]}
@@ -22,7 +20,7 @@
                         (food/get-nutrient-measurement food)
                         :measurement/quantity
                         str)
-             :class "mmm-tar"}])})
+             :class "mmm-tar mmm-nbr"}])})
 
 (defn render-nutrient-foods-table
   "Really, ALL of the foods on one page? Well, not all of them, just the ones that
@@ -38,14 +36,9 @@
 
   Thus: list of all foods containing the nutrient in question."
   [nutrient locale]
-  (let [foods (nutrient/get-foods-by-nutrient-density nutrient)]
-    (list
-     [:h2.mmm-h2 [:i18n ::foods-by-nutrient
-                  (str/lower-case (get (nutrient/get-name nutrient) locale))]]
-     [:p.mmm-p [:i18n ::foods {:n (count foods)}]]
-     (->> foods
-          (prepare-foods-table nutrient locale)
-          food-page/render-table))))
+  (->> (nutrient/get-foods-by-nutrient-density nutrient)
+       (prepare-foods-table nutrient locale)
+       food-page/render-table))
 
 (defn render [context _db page]
   (let [nutrient (d/entity (:foods/db context) [:nutrient/id (:page/nutrient-id page)])
@@ -69,23 +62,8 @@
            (when-let [desc (get-in nutrient/descriptions [(:nutrient/id nutrient) locale])]
              [:p desc])]]]]
 
-       [:div.mmm-container.mmm-section.mmm-mobile-phn
-        [:div.mmm-passepartout
-         (if-let [nutrients (seq (:nutrient/_parent nutrient))]
-           [:div.mmm-container
-            [:div.mmm-media.mmm-media-at.mmm-media-reverse
-             [:article.mmm-vert-layout-m
-              (render-nutrient-foods-table nutrient locale)]
-             [:aside.mmm-vert-layout-m.mmm-mtxl
-              {:style {:flex-basis "300px"}}
-              [:h3.mmm-h3 [:i18n ::sub-groups]]
-              [:ul.mmm-ul.mmm-unadorned-list
-               (for [n (nutrient/sort-by-preference nutrients)]
-                 [:li
-                  [:a.mmm-link {:href (urls/get-nutrient-url locale n)}
-                   [:i18n :i18n/lookup (nutrient/get-name n)]]])]]]]
-           [:div.mmm-container-medium.mmm-vert-layout-m
-            (render-nutrient-foods-table nutrient locale)])]]
+       [:div.mmm-container-medium.mmm-section.mmm-vert-layout-m
+        (render-nutrient-foods-table nutrient locale)]
 
        [:div.mmm-container.mmm-section
         (CompactSiteFooter)]]]]))
@@ -100,4 +78,4 @@
        count)
 
 
-)
+  )
