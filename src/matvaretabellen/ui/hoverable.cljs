24583209736b9e27f384b60713f1dd0a75d469f0
Magnar Sveen <magnar.sveen@mattilsynet.no>
2023-11-09 11:28:20 +0100

    Et nytt herlig forsøk på å kunne bruke kakediagrammer på mobil 🍰

--- src/matvaretabellen/ui/hoverable.cljs
+++ src/matvaretabellen/ui/hoverable.cljs
@@ -1,35 +1,52 @@
 (ns matvaretabellen.ui.hoverable)
 
 (defn get-hover-target [component event]
-  (.getElementById
+  (.querySelector
    component
-   (.getAttribute (.-target event) "data-hover_target_id")))
+   (str "#" (.getAttribute (.-target event) "data-hover_target_id"))))
+
+(defn ready-for-change? [element]
+  (let [now (.getTime (js/Date.))
+        ready? (if-let [then (some-> (.getAttribute element "data-throttle_ms")
+                                     parse-long)]
+                 (< 200 (- now then))
+                 true)]
+    (when ready?
+      (.setAttribute element "data-throttle_ms" now))
+    ready?))
 
 (defn show-target [component event]
-  (let [target (get-hover-target component event)
-        size (.getBoundingClientRect target)
-        container (.-parentElement target)
-        bounds (.getBoundingClientRect container)
-        cx-ratio (.getAttribute (.-target event) "data-hover_cx_ratio")
-        cy-ratio (.getAttribute (.-target event) "data-hover_cy_ratio")]
-    (set! (.-top (.-style target)) (str (- (* cy-ratio (.-height bounds))
-                                           (.-height size)
-                                           10) ;; height of arrow
-                                        "px"))
-    (set! (.-left (.-style target)) (str (- (* cx-ratio (.-width bounds))
-                                            (/ (.-width size) 2)) "px"))
-    (.add (.-classList target) "mtv-hover-popup-visible")))
+  (when (ready-for-change? (.-target event))
+    (let [target (get-hover-target component event)
+          size (.getBoundingClientRect target)
+          container (.-parentElement target)
+          bounds (.getBoundingClientRect container)
+          cx-ratio (.getAttribute (.-target event) "data-hover_cx_ratio")
+          cy-ratio (.getAttribute (.-target event) "data-hover_cy_ratio")]
+      (set! (.-top (.-style target)) (str (- (* cy-ratio (.-height bounds))
+                                             (.-height size)
+                                             10) ;; height of arrow
+                                          "px"))
+      (set! (.-left (.-style target)) (str (- (* cx-ratio (.-width bounds))
+                                              (/ (.-width size) 2)) "px"))
+      (.add (.-classList target) "mtv-hover-popup-visible"))))
 
 (defn hide-target [component event]
   (-> (get-hover-target component event)
       .-classList
       (.remove "mtv-hover-popup-visible")))
 
+(defn toggle-target [component event]
+  (when (ready-for-change? (.-target event))
+    (if (-> (get-hover-target component event)
+            .-classList
+            (.contains "mtv-hover-popup-visible"))
+      (hide-target component event)
+      (show-target component event))))
+
 (defn set-up [component]
   (when component
     (doseq [element (.querySelectorAll component ".js-hoverable")]
       (.addEventListener element "mouseenter" #(show-target component %) false)
       (.addEventListener element "mouseleave" #(hide-target component %) false)
-      ;; apparently mobile browsers will simulate these events when clicking inside and outside an element,
-      ;; so I have removed the click handler that used to be here
-      )))
+      (.addEventListener element "click" #(toggle-target component %) false))))
