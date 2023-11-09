532d7b14541f7844c92bc83df6b201b7d3b8f6bd
Magnar Sveen <magnar.sveen@mattilsynet.no>
2023-11-09 13:23:58 +0100

    List ut alle matvarene per matvaregruppe

--- src/matvaretabellen/pages/food_group_page.clj
+++ src/matvaretabellen/pages/food_group_page.clj
@@ -1,16 +1,28 @@
 (ns matvaretabellen.pages.food-group-page
   (:require [datomic-type-extensions.api :as d]
             [matvaretabellen.crumbs :as crumbs]
+            [matvaretabellen.pages.food-page :as food-page]
             [matvaretabellen.urls :as urls]
             [mmm.components.breadcrumbs :refer [Breadcrumbs]]
             [mmm.components.footer :refer [CompactSiteFooter]]
             [mmm.components.site-header :refer [SiteHeader]]))
+
+(defn get-all-foods [food-group]
+  (apply concat (:food/_food-group food-group)
+         (map get-all-foods (:food-group/_parent food-group))))
+
+(defn prepare-foods-table [locale foods]
+  {:headers [{:text "Matvare"}]
+   :rows (for [food foods]
+           [{:text [:a.mmm-link {:href (urls/get-food-url locale food)}
+                    [:i18n :i18n/lookup (:food/name food)]]}])})
 
 (defn render [context _db page]
   (let [food-group (d/entity (:foods/db context)
                              [:food-group/id (:page/food-group-id page)])
         details (d/entity (:app/db context)
                           [:food-group/id (:page/food-group-id page)])
+        foods (get-all-foods food-group)
         locale (:page/locale page)]
     [:html {:class "mmm"}
      [:body
@@ -27,11 +39,16 @@
          [:article.mmm-vert-layout-m
           [:div [:h1.mmm-h1 (get-in food-group [:food-group/name locale])]
            [:i18n :i18n/number-of-foods
-            {:count (count (:food/_food-group food-group))}]]
+            {:count (count foods)}]]
           [:div.mmm-text.mmm-preamble
            [:p (get-in details [:food-group/short-description locale])]]]
          [:aside.mmm-desktop {:style {:flex-basis "40%"}}
           [:img {:src (:food-group/illustration details)
                  :width 300}]]]]]
+
+      [:div.mmm-container-medium.mmm-section.mmm-vert-layout-m
+       (->> (prepare-foods-table locale foods)
+            food-page/render-table)]
+
       [:div.mmm-container.mmm-section
        (CompactSiteFooter)]]]))
