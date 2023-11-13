98fea17d7deea7f8a039717174a7f9ecbef16f03
Christian Johansen <christian.johansen@mattilsynet.no>
2023-11-13 20:56:26 +0100

    Rydd opp i feilende tester

--- test/matvaretabellen/pages/food_page_test.clj
+++ test/matvaretabellen/pages/food_page_test.clj
@@ -10,7 +10,7 @@
             {:decimals 2})
            '([:span {:data-portion "1295.67"
                      :data-decimals "2"}
-              "1295.67"] " kJ"))))
+              "1295.67"] " " [:span.mvt-sym "kJ"]))))
 
   (testing "Always rounds whole numbers to 0 decimals"
     (is (= (sut/get-calculable-quantity
@@ -18,12 +18,12 @@
             {:decimals 2})
            '([:span {:data-portion "1295"
                      :data-decimals "2"}
-              "1295"] " kJ"))))
+              "1295"] " " [:span.mvt-sym "kJ"]))))
 
   (testing "Defaults to 1 decimal"
     (is (= (sut/get-calculable-quantity
             {:measurement/quantity (misc/kilojoules 1295.670427)})
-           '([:span {:data-portion "1295.7"} "1295.7"] " kJ")))))
+           '([:span {:data-portion "1295.7"} "1295.7"] " " [:span.mvt-sym "kJ"])))))
 
 (deftest energy-test
   (testing "Controls kJ decimals"
@@ -31,7 +31,8 @@
             {:food/energy
              {:measurement/quantity (misc/kilojoules 1234.56)}})
            '([:span {:data-portion "1235"
-                     :data-decimals "0"} "1235"] " kJ"))))
+                     :data-decimals "0"
+                     :class "mvt-kj"} "1235"] " " [:span.mvt-sym "kJ"]))))
 
   (testing "Controls kcal decimals"
     (is (= (sut/energy
@@ -40,7 +41,10 @@
              :food/calories
              {:measurement/observation "234"}})
            '([:span {:data-portion "1235"
-                     :data-decimals "0"} "1235"]
-             " kJ"
+                     :data-decimals "0"
+                     :class "mvt-kj"} "1235"]
+             " " [:span.mvt-sym "kJ"]
              " (" [:span {:data-portion "234"
-                          :data-decimals "0"} "234"] " kcal" ")")))))
+                          :data-decimals "0"
+                          :class "mvt-kcal"} "234"]
+             " kcal" ")")))))
