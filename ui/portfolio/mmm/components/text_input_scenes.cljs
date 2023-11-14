f7ce6f9b0ce3dafef2273a120076ea04b3433c13
Christian Johansen <christian.johansen@mattilsynet.no>
2023-11-14 16:07:09 +0100

    Pirk litt på høyden på knapper og input-felter så de passer sammen

--- ui/portfolio/mmm/components/text_input_scenes.cljs
+++ ui/portfolio/mmm/components/text_input_scenes.cljs
@@ -1,5 +1,7 @@
 (ns mmm.components.text-input-scenes
-  (:require [mmm.components.text-input :refer [TextInput]]
+  (:require [mmm.components.button :refer [Button]]
+            [mmm.components.select :refer [Select]]
+            [mmm.components.text-input :refer [TextInput]]
             [mmm.elements :as e]
             [portfolio.dumdom :as portfolio :refer [defscene]]))
 
@@ -8,3 +10,11 @@
 
 (defscene text-input
   (e/block (TextInput {})))
+
+(defscene form-inputs
+  "Det er rimelig å forvente at skjema-elementene tar samme vertikale plass og
+  ser ålreit ut sammen"
+  [:div.mmm-flex.mmm-flex-gap
+   (TextInput {})
+   (Select {})
+   (Button {:text "Snurr film!"})])
