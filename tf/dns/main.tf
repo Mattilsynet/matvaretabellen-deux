6e8f624d1bc993375a55dd24c97416f63f653eb9
Christian Johansen <christian.johansen@mattilsynet.no>
2023-11-15 10:24:19 +0100

    Faktisk kjørbar terraform!

--- tf/dns/main.tf
+++ tf/dns/main.tf
@@ -1,6 +1,6 @@
-resource "google_dns_managed_zone" "matvaretabellen_dns_zone" {
-  name = "matvaretabellen-dns-managed-zone"
-  dns_name = "matvaretabellen.no"
+resource "google_dns_managed_zone" "mvt_dns_zone" {
+  name = "matvaretabellen-dns-zone"
+  dns_name = "matvaretabellen.no."
   project = var.project_id
   description = "Matvaretabellen"
   dnssec_config {
@@ -9,17 +9,19 @@ resource "google_dns_managed_zone" "matvaretabellen_dns_zone" {
 }
 
 resource "google_dns_record_set" "matvaretabellen" {
-  name = data.google_dns_managed_zone.matvaretabellen_dns_zone.dns_name
-  managed_zone = data.google_dns_managed_zone.matvaretabellen_dns_zone.name
+  name = "${resource.google_dns_managed_zone.mvt_dns_zone.dns_name}"
+  managed_zone = resource.google_dns_managed_zone.mvt_dns_zone.name
   type = "A"
   ttl = 300
+  project = var.project_id
   rrdatas = ["194.19.30.143"]
 }
 
 resource "google_dns_record_set" "www-matvaretabellen" {
-  name = "www.${data.google_dns_managed_zone.matvaretabellen_dns_zone.dns_name}"
-  managed_zone = data.google_dns_managed_zone.matvaretabellen_dns_zone.name
+  name = "www.${resource.google_dns_managed_zone.mvt_dns_zone.dns_name}"
+  managed_zone = resource.google_dns_managed_zone.mvt_dns_zone.name
   type = "A"
   ttl = 300
+  project = var.project_id
   rrdatas = ["194.19.30.143"]
 }
