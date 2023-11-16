f04b8a29ae071721838fbde751cfe9a88a4b7966
Christian Johansen <christian.johansen@mattilsynet.no>
2023-11-16 10:57:19 +0100

    Fiks testene!

--- test/matvaretabellen/pages/food_page_test.clj
+++ test/matvaretabellen/pages/food_page_test.clj
@@ -9,6 +9,7 @@
             {:food/energy
              {:measurement/quantity (misc/kilojoules 1234.56)}})
            '([:span {:data-portion "1234.56"
+                     :data-value "1234.56"
                      :data-decimals "0"
                      :class "mvt-kj"}
               [:i18n :i18n/number {:n 1234.56, :decimals 0}]] " "
@@ -21,11 +22,13 @@
              :food/calories
              {:measurement/observation "234"}})
            '([:span {:data-portion "1234.56"
+                     :data-value "1234.56"
                      :data-decimals "0"
                      :class "mvt-kj"}
               [:i18n :i18n/number {:n 1234.56, :decimals 0}]] " "
              [:span.mvt-sym "kJ"]
              " (" [:span {:data-portion "234"
+                          :data-value "234"
                           :data-decimals "0"
                           :class "mvt-kcal"}
                    [:i18n :i18n/number {:n 234, :decimals 0}]]
