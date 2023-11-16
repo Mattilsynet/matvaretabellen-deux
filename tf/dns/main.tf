01dbdc767f59830d5403b04e83ae6fc93e2c7e97
Christian Johansen <christian.johansen@mattilsynet.no>
2023-11-16 13:29:19 +0100

    Pek matvaretabellen.no til ny løsning

--- tf/dns/main.tf
+++ tf/dns/main.tf
@@ -7,3 +7,21 @@ resource "google_dns_managed_zone" "mvt_dns_zone" {
     state = "on"
   }
 }
+
+resource "google_dns_record_set" "matvaretabellen" {
+  name = "${resource.google_dns_managed_zone.mvt_dns_zone.dns_name}"
+  managed_zone = resource.google_dns_managed_zone.mvt_dns_zone.name
+  type = "A"
+  ttl = 300
+  project = var.project_id
+  rrdatas = ["34.36.51.153"]
+}
+
+resource "google_dns_record_set" "www-matvaretabellen" {
+  name = "www.${resource.google_dns_managed_zone.mvt_dns_zone.dns_name}"
+  managed_zone = resource.google_dns_managed_zone.mvt_dns_zone.name
+  type = "A"
+  ttl = 300
+  project = var.project_id
+  rrdatas = ["34.36.51.153"]
+}
