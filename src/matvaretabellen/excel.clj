467c8107d1087f6a631dae217d6649b14d4ab5e7
Magnar Sveen <magnar.sveen@mattilsynet.no>
2023-11-20 10:09:43 +0100

    Fiks desimalpresisjon på Excel-ark

--- src/matvaretabellen/excel.clj
+++ src/matvaretabellen/excel.clj
@@ -15,7 +15,7 @@
 ;; time on the data files. In other words: code changes will not automatically
 ;; be reflected in the export. Change this version string when you need for new
 ;; Excel files to be generated.
-(def version "2023.11.19")
+(def version "2023.11.20")
 
 (defn add-index [coll]
   (map-indexed (fn [i m] (assoc m :index i)) coll))
@@ -121,7 +121,7 @@
 (defn get-measurement-number [measurement field & [decimal-precision]]
   (let [fix (if decimal-precision
               #(to-fixed % decimal-precision)
-              #(to-nutrient-decimal-precision % (:measurement/nutrient measurement)))]
+              #(to-nutrient-decimal-precision % (:constituent/nutrient measurement)))]
     (case field
       :measurement/percent (str (:measurement/percent measurement))
       :measurement/observation (if-let [num (parse-long (:measurement/observation measurement))]
