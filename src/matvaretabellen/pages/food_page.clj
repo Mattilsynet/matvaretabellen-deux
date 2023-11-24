fbd7ee511c25e6afc81cad27adfab598ddc52f23
Christian Johansen <christian.johansen@mattilsynet.no>
2023-11-24 11:44:48 +0100

    Vis andre lignende matvarer i toppen av matvaresiden

--- src/matvaretabellen/pages/food_page.clj
+++ src/matvaretabellen/pages/food_page.clj
@@ -441,6 +441,20 @@
                  (map DetailFocusCard))]]
           [:div.mmm-mobile.mmm-mtm (render-compare-button food {:inline? false})]]
          (render-toc {:contents (get-toc-items)})]]]
+
+      (when-let [related (seq (food/find-related-foods food locale))]
+        (let [categoryish (food/infer-food-kind food locale)]
+          [:div.mmm-container.mmm-section.mmm-inline
+           [:strong [:i18n ::more {:categoryish (str/lower-case categoryish)}] " "]
+           [:ul.mmm-horizontal-list.mmm-hl-slash
+            (for [food related]
+              [:li
+               [:span
+                [:a.mmm-link {:href (urls/get-food-url locale food)
+                              :data-comparison-suggestion-id (:food/id food)
+                              :data-comparison-suggestion-name (get-in food [:food/name locale])}
+                 (food/get-variant-name food locale categoryish)]]])]]))
+
       [:div.mmm-container.mmm-section
        [:div.mmm-flex-desktop.mmm-flex-bottom.mmm-mbl
         [:h2.mmm-h2.mmm-mbn#naringsinnhold [:i18n ::nutrition-title]]
