64cf0141bbbda51fcfcd182ad9f511b0b73dd072
Christian Johansen <christian.johansen@mattilsynet.no>
2023-11-27 13:55:26 +0100

    Hvis toggleren er en knapp, toggle den mellom sekundær og primær ved av/på

--- src/matvaretabellen/ui/toggler.cljs
+++ src/matvaretabellen/ui/toggler.cljs
@@ -1,14 +1,22 @@
 (ns matvaretabellen.ui.toggler
   (:require [matvaretabellen.ui.dom :as dom]))
 
+(defn get-target [el]
+  (if-let [selector (.getAttribute el "data-toggle-target")]
+    [el (dom/qs selector)]
+    (some-> el .-parentNode get-target)))
+
 (defn init-toggler [toggler]
   (->> (fn [e]
-         (when-let [target (some-> (.-target e)
-                                   (.getAttribute "data-toggle-target")
-                                   dom/qs)]
-           (if (dom/has-class target "mmm-hidden")
-             (dom/show target)
-             (dom/hide target))))
+         (when-let [[el target] (get-target (.-target e))]
+           (let [hidden? (dom/has-class target "mmm-hidden")]
+             (when (and hidden? (dom/has-class el "mmm-button-secondary"))
+               (dom/remove-class el "mmm-button-secondary"))
+             (when (and (not hidden?) (dom/has-class el "mmm-button"))
+               (dom/add-class el "mmm-button-secondary"))
+             (if hidden?
+               (dom/show target)
+               (dom/hide target)))))
        (.addEventListener toggler "click")))
 
 (defn init []
