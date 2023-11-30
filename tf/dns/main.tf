cc068cdb7d650714e7db1506f4c7ae8868198e43
Christian Johansen <christian.johansen@mattilsynet.no>
2023-11-30 12:51:01 +0100

    Legg til DNS-oppføringer for ipv6

--- tf/dns/main.tf
+++ tf/dns/main.tf
@@ -17,6 +17,15 @@ resource "google_dns_record_set" "matvaretabellen" {
   rrdatas = ["34.36.135.119"]
 }
 
+resource "google_dns_record_set" "matvaretabellen-ipv6" {
+  name = "${resource.google_dns_managed_zone.mvt_dns_zone.dns_name}"
+  managed_zone = resource.google_dns_managed_zone.mvt_dns_zone.name
+  type = "AAAA"
+  ttl = 300
+  project = var.project_id
+  rrdatas = ["2600:1901:0:ecf6::"]
+}
+
 resource "google_dns_record_set" "www-matvaretabellen" {
   name = "www.${resource.google_dns_managed_zone.mvt_dns_zone.dns_name}"
   managed_zone = resource.google_dns_managed_zone.mvt_dns_zone.name
@@ -24,6 +33,15 @@ resource "google_dns_record_set" "www-matvaretabellen" {
   ttl = 300
   project = var.project_id
   rrdatas = ["34.36.135.119"]
+}
+
+resource "google_dns_record_set" "www-matvaretabellen-ipv6" {
+  name = "www.${resource.google_dns_managed_zone.mvt_dns_zone.dns_name}"
+  managed_zone = resource.google_dns_managed_zone.mvt_dns_zone.name
+  type = "AAAA"
+  ttl = 300
+  project = var.project_id
+  rrdatas = ["2600:1901:0:ecf6::"]
 }
 
 resource "google_dns_record_set" "empty_spf1_txt_record" {
