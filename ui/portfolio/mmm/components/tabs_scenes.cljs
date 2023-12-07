5bc281081afd240ee505f40276c2cb69fb622a2c
Christian Johansen <christian.johansen@mattilsynet.no>
2023-12-07 14:13:38 +0100

    Legg til en pille-variant av tabs-komponenten

--- ui/portfolio/mmm/components/tabs_scenes.cljs
+++ ui/portfolio/mmm/components/tabs_scenes.cljs
@@ -1,5 +1,5 @@
 (ns mmm.components.tabs-scenes
-  (:require [mmm.components.tabs :refer [Tabs]]
+  (:require [mmm.components.tabs :refer [Tabs PillTabs]]
             [mmm.elements :as e]
             [portfolio.dumdom :as portfolio :refer [defscene]]))
 
@@ -15,3 +15,17 @@
     {:tabs [{:text "Kakediagram" :selected? true}
             {:text "Tabell"}
             {:text "Banan"}]})))
+
+(def pill-tab-component
+  (PillTabs
+   {:tabs [{:text "Kakediagram" :selected? true}
+           {:text "Tabell"}
+           {:text "Banan"}]}))
+
+(defscene pill-tabs
+  (e/block pill-tab-component))
+
+(defscene pill-tabs-different-themes
+  [:div.mmm-block.mmm-vert-layout-m
+   [:div.mmm-brand-theme2 pill-tab-component]
+   [:div.mmm-brand-theme3 pill-tab-component]])
--- ui/src/mmm/components/tabs.cljc
+++ ui/src/mmm/components/tabs.cljc
@@ -1,9 +1,12 @@
 (ns mmm.components.tabs)
 
-(defn Tabs [{:keys [tabs]}]
-  [:div.mmm-tabs
+(defn Tabs [{:keys [tabs class]}]
+  [:div.mmm-tabs {:class class}
    (for [{:keys [selected? text id]} tabs]
      [:div.tab
       {:class (when selected? :selected)
        :id id}
       text])])
+
+(defn PillTabs [params]
+  (Tabs (assoc params :class [:mmm-tabs-pill-mode])))
