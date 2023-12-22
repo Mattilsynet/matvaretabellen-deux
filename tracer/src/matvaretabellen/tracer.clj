4bdd30a9ff09afb6d43ce95fde960b3f6e7600ea
Magnar Sveen <magnar.sveen@mattilsynet.no>
2023-12-22 09:51:54 +0100

    Ta imot feilmeldinger på enkleste mulige måte i tracer

--- tracer/src/matvaretabellen/tracer.clj
+++ tracer/src/matvaretabellen/tracer.clj
@@ -2,6 +2,7 @@
   (:gen-class)
   (:require [clojure.pprint :as pprint]
             [dumdom.string :as dumdom]
+            [lambdaisland.uri :refer [uri query-map]]
             [org.httpkit.server :as http-kit]))
 
 (defonce store (atom {}))
@@ -13,21 +14,32 @@
    (dumdom/render
     [:div
      [:h1 "Sporing av feilsituasjoner på Matvaretabellen"]
+     [:p "Her er det strikk og binders, og vi er helt tomme for strikk. Slenger
+     ut noen følere for å se etter problemer med klienten i jula. God jul, da!"]
      [:pre (with-out-str (pprint/pprint @store))]])})
 
 (defn ++ [n]
   (if (nil? n) 1 (inc n)))
 
 (defn handler [req]
-  (cond
-    (= "/tracer/no-script/" (:uri req))
-    (do (swap! store update "No script" ++)
+  (let [uri (uri (:uri req))] ;; Bjarne, Bjarne, Bjarne
+    (case (:path uri)
+      "/tracer/no-script/"
+      (do (swap! store update "No script" ++)
+          {:status 200 :body "OK"})
+
+      "/tracer/report/"
+      (let [error (or (:error (query-map (:uri req)))
+                      "Overraskende ingen error på rapporten")]
+        (swap! store update error ++)
         {:status 200 :body "OK"})
 
-    (= "/tracer/infos/" (:uri req))
-    (render-infos)
+      "/tracer/infos/"
+      (render-infos)
 
-    :else {:status 200 :body "OK"})) ;; ikke noe stress, alt er greit!
+      ;; bomma litt?
+      (do (swap! store assoc "Traff ikke i case'n, gett" (:path uri))
+          {:status 200 :body "OK"}))))
 
 (defn start-server [port]
   (http-kit/run-server handler {:port port}))
