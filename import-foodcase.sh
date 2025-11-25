47c7eda48aacbe4133c89f8f8c7d3eb3e1d97a0c
Teodor Heggelund <teodor.lunaas.heggelund@mattilsynet.no>
2025-11-25 16:49:36 +0100

    Legg til zippet XML med info om FoodEx2

--- import-foodcase.sh
+++ import-foodcase.sh
@@ -19,3 +19,8 @@ curl -f https://foodcase.prod.nfsa.foodcase-services.com/FoodCASE_WebAppMattilsy
 curl -f https://foodcase.prod.nfsa.foodcase-services.com/FoodCASE_WebAppMattilsynet/ws/dataexport/food_english -H "Authorization: Bearer $fc_bearer" | jq '.' > data/foodcase-food-en.json
 curl -f https://foodcase.prod.nfsa.foodcase-services.com/FoodCASE_WebAppMattilsynet/ws/dataexport/data_norwegian -H "Authorization: Bearer $fc_bearer" | jq '.' > data/foodcase-data-nb.json
 curl -f https://foodcase.prod.nfsa.foodcase-services.com/FoodCASE_WebAppMattilsynet/ws/dataexport/data_english -H "Authorization: Bearer $fc_bearer" | jq '.' > data/foodcase-data-en.json
+
+# Etter overgangen til FoodEx2 (se doc/foodex2.md), trenger vi også en
+# XML-datadump for å forstå FoodEx2-koder.
+mv data/MTX.ecf data/MTX.old.ecf
+curl -o data/MTX.ecf https://github.com/openefsa/efsa-catalogues/raw/bbc4e2008f2541b2f3a5e975ebc37d42e71da276/ecf_catalogues/MTX.ecf
