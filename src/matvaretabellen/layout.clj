9ff51a338f2136c721ee44dfe2f43d7d75740819
Christian Johansen <christian.johansen@mattilsynet.no>
2025-12-06 08:59:58 +0100

    Inline headeren i layout-navnerommet

--- src/matvaretabellen/layout.clj
+++ src/matvaretabellen/layout.clj
@@ -4,8 +4,7 @@
             [matvaretabellen.crumbs :as crumbs]
             [matvaretabellen.ui.breadcrumbs :refer [Breadcrumbs]]
             [matvaretabellen.ui.search-input :refer [SearchInput]]
-            [matvaretabellen.urls :as urls]
-            [mmm.components.site-header :refer [SiteHeader]]))
+            [matvaretabellen.urls :as urls]))
 
 (def report-errors-to-tracer
   "
@@ -103,6 +102,23 @@ window.onerror = function(message) {
       (cond-> link
         (= current-url (:url link))
         (dissoc :url)))))
+
+(defn SiteHeader [{:keys [home-url extra-link extra-links]}]
+  [:header.header
+   [:div {:class (mtds/classes :flex) :data-center "xl" :data-justify "space-between" :data-align "center"}
+    [:a {:class (mtds/classes :logo)
+         :href home-url}
+     "Matvaretabellen"]
+    (when extra-link
+      [:a {:href (:url extra-link)}
+       (:text extra-link)])
+    (when extra-links
+      [:menu {:class (mtds/classes :flex) :data-gap "8"}
+       (for [{:keys [url text class]} extra-links]
+         [:li {:class class}
+          (if url
+            [:a {:href url} text]
+            text)])])]])
 
 (defn render-header [{:keys [locale app/config]} get-current-url]
   [:div#header
